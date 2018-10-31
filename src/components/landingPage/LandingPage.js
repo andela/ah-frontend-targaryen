a8b38b3061d46a5f8fd7a89d29e367d619a0f56a
RonKbS <stephen.bamwite@gmail.com>
2018-10-31 19:59:29 +0300

    feat(landing-page): create a landing page
    (fix)display a landing page and navbar on navigating to main url
    
    [Delivers #161541575]

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,32 +1,44 @@
 import React from 'react';
 
 const DisplayArticles = () => (
-  <div className="article">
-    <div className="article-content float-sm-bottom LP-list">
-      <a href="#list-articles">
-        List Articles Place Holder
-      </a>
+  <div className="card">
+    {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
+    <div className="card-body">
+      <h5 className="card-title">Nisi cupidatat</h5>
+      <p className="card-text">
+        Nisi cupidatat magna dolor nulla.
+        Incididunt labore aliqua cillum mollit quis tempor do pariatur veniam adipisicing.
+        Commodo sit voluptate est laboris irure ex dolor fugiat esse ea mollit.
+        Pariatur elit cillum commodo occaecat tempor non mollit eu enim velit
+        <br />
+        . . .
+      </p>
+      <a href="#" className="btn ah-btn">View more</a>
     </div>
   </div>
 );
 
 const LandingPage = () => (
   <React.Fragment>
-    <div className="container LP-container">
-      <div className="row jumbotron mx-auto LP-text">
-        <h1>
-          Authors Haven is the one stop hub for knowledge.
-          <br />
-          Come take a look at thousands of articles.
-          <br />
-          You can even share your own!!!
-        </h1>
-        <a href="#start" className="btn btn-outline btn-xl js-scroll-trigger join-us-text">
-          Join us and share your ideas
-        </a>
+    <div className="first-section">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 jumbotron mx-auto lp-intro lp-text">
+            <p>Authors Haven is the one stop hub for knowledge.</p>
+            <p>Come take a look at thousands of articles.</p>
+            <p>You can even share your own!!!</p>
+            <a href="#start" className="btn ah-btn">
+              Join us and share your ideas
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="display-articles mx-auto">
-        <DisplayArticles />
+    </div>
+    <div className="container">
+      <div className="display-articles mx-auto row">
+        <div className="col-sm-12 col-4 col-md-6"><DisplayArticles /></div>
+        <div className="col-sm-12 col-4 col-md-6"><DisplayArticles /></div>
+        <div className="col-sm-12 col-4 col-md-6"><DisplayArticles /></div>
       </div>
     </div>
   </React.Fragment>
