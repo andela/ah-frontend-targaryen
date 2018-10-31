c47892f24072dcf82715d36f5314d47ab9f50693
mendozabree <mendozabridget777@gmail.com>
2018-10-31 07:50:29 +0300

    chore(sass): set up sass
    this chore will install setup sass so one can run both .css and .scss files
    
    [Delivers #160609511]changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Routes from './components/routes';
-import './App.css';
 import store from './store';
+import './assets/App.scss';
+import './assets/App.css';
 
 const App = () => (
   <Provider store={store}>
